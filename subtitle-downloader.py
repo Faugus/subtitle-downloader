d07d818bc666ae6ed7b9cd956e806f0cb0d9708b
Faugus <felix.ribeiro@live.com>
2026-01-11 20:39:32 -0300

    Update subtitle-downloader.py

--- subtitle-downloader.py
+++ subtitle-downloader.py
@@ -21,6 +21,20 @@ from pathlib import Path
 
 class PathManager:
     @staticmethod
+    def system_data(*relative_paths):
+        xdg_data_dirs = os.getenv('XDG_DATA_DIRS', '/usr/local/share:/usr/share').split(':')
+        for data_dir in xdg_data_dirs:
+            path = Path(data_dir).joinpath(*relative_paths)
+            if path.exists():
+                return str(path)
+        return str(Path(xdg_data_dirs[0]).joinpath(*relative_paths))
+
+    @staticmethod
+    def user_data(*relative_paths):
+        xdg_data_home = Path(os.getenv('XDG_DATA_HOME', Path.home() / '.local/share'))
+        return str(xdg_data_home.joinpath(*relative_paths))
+
+    @staticmethod
     def user_config(*relative_paths):
         xdg_config_home = Path(os.getenv('XDG_CONFIG_HOME', Path.home() / '.config'))
         return str(xdg_config_home.joinpath(*relative_paths))
@@ -39,6 +53,7 @@ class PathManager:
 
 class SubtitleDownloader(QWidget):
     IS_FLATPAK = 'FLATPAK_ID' in os.environ or os.path.exists('/.flatpak-info')
+    CONFIG_DIR = PathManager.user_config('subtitle-downloader')
     CONFIG_FILE = PathManager.user_config('subtitle-downloader/config.ini')
     if IS_FLATPAK:
         ICON_FILE = PathManager.get_icon('io.github.Faugus.subtitle-downloader.png')
